17b07c6757246638c7fc8050e5ddc8031866d243
Oskar <oskar@wycislak.pl>
2018-10-23 18:19:25 +0200

    Use os.environ.get instead of direct access

--- d/settings.py
+++ d/settings.py
@@ -3,10 +3,10 @@ from .settings_common import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': os.environ['DATABASE_SERVICE_NAME'],
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_SERVICE_NAME'),
         'PORT': '5432',
     }
 }
