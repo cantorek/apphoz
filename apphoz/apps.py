cf087e2baea26c8df6eff1002f0b0541f6bd3170
Oskar <oskar@wycislak.pl>
2018-10-23 13:39:29 +0200

    Initial commit

---
+++ apphoz/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApphozConfig(AppConfig):
+    name = 'apphoz'
---
+++ apphoz/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from d.settings import JSON_TEMPLATE
+
+
+def template(request):
+    return JsonResponse(JSON_TEMPLATE)
